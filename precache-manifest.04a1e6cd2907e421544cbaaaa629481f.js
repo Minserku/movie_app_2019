37707b00b170601489e68bc493e0644167270a2e
Minserku <minserku>
2020-02-07 13:46:34 +0900

    Updates

---
+++ precache-manifest.04a1e6cd2907e421544cbaaaa629481f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203e5b72c2b9fc21b918dfa6fb22511c",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "de07b0322593299dd0ef",
+    "url": "/movie_app_2019/static/css/main.d19469cc.chunk.css"
+  },
+  {
+    "revision": "fe7e2cab0773743a5557",
+    "url": "/movie_app_2019/static/js/2.2df6c82d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2019/static/js/2.2df6c82d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de07b0322593299dd0ef",
+    "url": "/movie_app_2019/static/js/main.170ee30b.chunk.js"
+  },
+  {
+    "revision": "bc42afcb986eef44e7a7",
+    "url": "/movie_app_2019/static/js/runtime-main.30c24a73.js"
+  }
+]);
